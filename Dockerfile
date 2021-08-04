ed12926d34dba8ae69ea3e86cddb466869916b58
Mark <mcrutherford81@gmail.com>
2021-08-04 19:47:55 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM gorialis/discord.py:3.9.4-buster-pypi-minimal
+
+WORKDIR /app
+
+COPY requirements.txt ./
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "homebot.py"]
---
+++ app/homebot.py
@@ -0,0 +1,14 @@
+"""
+File: homebot.py
+Author: Mark Rutherford
+Created: 8/4/2021 6:34 PM
+"""
+import discord
+
+
+def main():
+    print(discord.version_info)
+
+
+if __name__ == '__main__':
+    main()
