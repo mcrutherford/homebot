3254534c6502f49c97e62587208c6656a2ea78eb
Mark <mcrutherford81@gmail.com>
2021-08-04 21:10:56 -0400

    Still getting docker working

--- homebot/homebot.py
+++ homebot/homebot.py
@@ -3,13 +3,18 @@ File: homebot.py
 Author: Mark Rutherford
 Created: 8/4/2021 6:34 PM
 """
+import os
+
 import discord
-import sayhello
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.environ['DISCORD_TOKEN'] or os.getenv('DISCORD_TOKEN')
 
 
 def main():
     print(discord.version_info)
-    sayhello.say_hi()
+    print(TOKEN)
 
 
 if __name__ == '__main__':
